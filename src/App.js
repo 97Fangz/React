9f7881cd4ab6d81310c8d2b1b06b597505d539b7
97Fangz <briansegem25@gmail.com>
2024-06-04 11:56:43 +0300

    "r"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloaded by .
+          Edit <code>src/App.js</code> and save to reloaded by Brian.
         </p>
         <a
           className="App-link"
